27f426eb7aa516ea0991605d093681af7cd34cad
ZeroEggVoid <ZeroEggVoid@github.cn>
2025-12-09 21:30:09 +0800

    FirstCheck

--- Project/user/main.c
+++ Project/user/main.c
@@ -1,19 +1,19 @@
-
+// FirstCheck
 #include "zf_common_headfile.h"
 
 void main()
 {
-    clock_init(SYSTEM_CLOCK_30M);
+	clock_init(SYSTEM_CLOCK_30M);
 	debug_init();
 	Motor_PWM_Init();
 	Servo_PWM_Init();
 	Encoder_Init();
 	LCD_Init();
-	
-    while(1)
-    {
-		Motor_Out(10,10);
-		Turn_Angle(90);	
+
+	while (1)
+	{
+		Motor_Out(10, 10);
+		Turn_Angle(90);
 		LCD_Disp();
-    }
+	}
 }
